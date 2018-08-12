dba969eed0405b679a76b57b82c95c272d31a0fb
HeadyT0pper <headyt0pper89@gmail.com>
2018-08-11 20:25:49 -0500

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+# Define a method, #introduction_with_language that takes in two arguments, name and language and outputs
+# the phrase: `"Hi, my name is #{name} and I am learning to program in #{language}."
